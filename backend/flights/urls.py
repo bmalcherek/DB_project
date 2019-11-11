6a53266f76505b4e1545184f29cc187d2f0d5fc5
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-11-11 18:22:56 +0100

    Added country detailed view in API

--- backend/flights/urls.py
+++ backend/flights/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('countries/', views.CountriesList.as_view()),
+    path('countries/<int:country_id>/', views.country_detail),
     path('airports/', views.AirportsList.as_view()),
     path('airplane-models/', views.AirplaneModelsList.as_view()),
     path('airplanes/', views.AirplanesList.as_view()),
@@ -12,5 +13,5 @@ urlpatterns = [
     path('crew-members/', views.CrewMembersList.as_view()),
     path('flights/', views.FlightsList.as_view()),
     path('reservations/', views.ReservationsList.as_view()),
-    path('luggages/', views.LuggagesList.as_view()) 
+    path('luggages/', views.LuggagesList.as_view())
 ]
--- backend/flights/views.py
+++ backend/flights/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework import generics, status
+from django.http import JsonResponse, HttpResponse
 
 from .serializers import (
     CountrySerializer,
@@ -30,6 +31,30 @@ from .models import (
 class CountriesList (generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def country_detail(request, country_id):
+    try:
+        country = Country.objects.get(id=country_id)
+    except Country.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = CountrySerializer(country)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CountrySerializer(country, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            print(serializer.errors)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        country.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 class AirportsList (generics.ListCreateAPIView):
@@ -74,4 +99,4 @@ class ReservationsList (generics.ListCreateAPIView):
 
 class LuggagesList (generics.ListCreateAPIView):
     queryset = Luggage.objects.all()
-    serializer_class =  LuggageSerializer
+    serializer_class = LuggageSerializer
