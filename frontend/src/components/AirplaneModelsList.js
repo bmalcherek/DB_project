f984b9d4017f689f23e576e88ed0d92257658016
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-11-12 19:31:43 +0100

    Switched from airplaneModels list to table with sorting

--- frontend/src/components/AirplaneModelsList.js
+++ frontend/src/components/AirplaneModelsList.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  List,
-  ListItem,
-  ListItemText,
   IconButton,
-  ListItemSecondaryAction
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableSortLabel
 } from "@material-ui/core";
 import { Edit, Delete } from "@material-ui/icons";
 
@@ -15,6 +17,8 @@ import { fetchData } from "../helpers";
 
 const AirplaneModelsList = () => {
   const [airplaneModels, setAirplaneModels] = useState([]);
+  const [orderBy, setOrderBy] = useState("name");
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
     const response = fetchData("api/airplane-models/");
@@ -26,28 +30,96 @@ const AirplaneModelsList = () => {
     console.log("implement delete");
   };
 
-  const airplaneModelsListElements = airplaneModels.map(airplaneModel => (
-    <ListItem button key={airplaneModel.id}>
-      <ListItemText
-        primary={airplaneModel.name}
-        secondary={`Manufacturer: ${airplaneModel.manufacturer}, Symbol: ${airplaneModel.symbol}`}
-      />
-      <ListItemSecondaryAction>
-        <IconButton edge="end">
+  const handleSort = key => () => {
+    let ord;
+    if (orderBy === key) {
+      ord = order === "desc" ? "asc" : "desc";
+      setOrder(ord);
+    } else {
+      ord = "desc";
+      setOrderBy(key);
+      setOrder("desc");
+    }
+    sort(airplaneModels, key, ord);
+  };
+
+  const sort = (models, by, ord) => {
+    setAirplaneModels(
+      models.sort((element1, element2) => {
+        if (ord === "asc") {
+          return element1[by] > element2[by]
+            ? 1
+            : element1[by] < element2[by]
+            ? -1
+            : 0;
+        } else {
+          return element1[by] > element2[by]
+            ? -1
+            : element1[by] < element2[by]
+            ? 1
+            : 0;
+        }
+      })
+    );
+  };
+
+  const airplaneModelsTableRows = airplaneModels.map(airplaneModel => (
+    <TableRow key={airplaneModel.id}>
+      <TableCell>{airplaneModel.name}</TableCell>
+      <TableCell align="right">{airplaneModel.manufacturer}</TableCell>
+      <TableCell align="right">{airplaneModel.symbol}</TableCell>
+      <TableCell align="right">
+        <IconButton>
           <Edit />
         </IconButton>
-        <IconButton edge="end" onClick={handleDelete} name={airplaneModel.id}>
-          <Delete name={airplaneModel.id} />
+        <IconButton onClick={handleDelete} name={airplaneModel.id}>
+          <Delete />
         </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+      </TableCell>
+    </TableRow>
   ));
 
-  console.log(airplaneModels);
-
   return (
-    <div id="airplane-models-list-container">
-      <List id="airplane-models-list">{airplaneModelsListElements}</List>
+    <div id="airplane-models-table-container">
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell key="name">
+              <TableSortLabel
+                active={orderBy === "name"}
+                onClick={handleSort("name")}
+                direction={order}
+              >
+                <b>Name</b>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="right" key="manufacturer">
+              <TableSortLabel
+                active={orderBy === "manufacturer"}
+                align="right"
+                onClick={handleSort("manufacturer")}
+                direction={order}
+              >
+                <b>Manufacturer</b>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="right" key="symbol">
+              <TableSortLabel
+                active={orderBy === "symbol"}
+                align="right"
+                onClick={handleSort("symbol")}
+                direction={order}
+              >
+                <b>Symbol</b>
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="right">
+              <b>Actions</b>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>{airplaneModelsTableRows}</TableBody>
+      </Table>
     </div>
   );
 };
