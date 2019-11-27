1be3b7f9a2633ef510b8fc61786006b86644af68
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-11-27 23:07:04 +0100

    Add AuthContext

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+	const [auth, setAuth] = useState(() => {
+		if (localStorage.getItem("stayAuth")) {
+			return true;
+		}
+		return false;
+	});
+	const [username, setUsername] = useState("");
+	const [stayAuth, setStayAuth] = useState(() => {
+		if (localStorage.getItem("stayAuth")) {
+			return true;
+		}
+		return false;
+	});
+
+	return (
+		<AuthContext.Provider
+			value={{ auth, setAuth, username, setUsername, stayAuth, setStayAuth }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+AuthProvider.propTypes = {
+	children: PropTypes.node.isRequired
+};
+
+export const useAuthValue = () => useContext(AuthContext);
