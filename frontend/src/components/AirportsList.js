657f2ebc44986baaf1de7ab78e2b308f9a96b5b6
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-12-06 18:45:04 +0100

    SWITCH to final design in airports list

--- frontend/src/components/AirportsList.js
+++ frontend/src/components/AirportsList.js
@@ -1,67 +1,122 @@
 import React, { useState, useEffect } from "react";
-import {
-	List,
-	ListItem,
-	ListItemText,
-	ListItemSecondaryAction,
-	IconButton,
-	Button
-} from "@material-ui/core";
-import { Edit, Delete } from "@material-ui/icons";
-import { Link } from "react-router-dom";
+import { Table, TableBody, TableCell, TableRow } from "@material-ui/core";
 
-import { fetchData, deleteItem } from "../helpers";
-
-//TODO: implement edit
+import { fetchData, listSort } from "../helpers";
+import { TableToolbar, TableHeader, TableActions } from "./table";
 
 const AirportList = () => {
-	const [airports, setAirtports] = useState([]);
+	const [airports, setAirports] = useState([]);
+	const [ogAirports, setOgAirports] = useState([]);
+	const [nameFilter, setNameFilter] = useState("");
+	const [icaoCodeFilter, setIcaoCodeFilter] = useState("");
+	const [iataCodeFilter, setIataCodeFilter] = useState("");
+	const [countryFilter, setCountryFilter] = useState("");
+	const [orderBy, setOrderBy] = useState("name");
+	const [order, setOrder] = useState("desc");
 	const [edited, setEdited] = useState(false);
 
 	useEffect(() => {
 		const response = fetchData("api/airports/");
 		response.then(res => {
-			setAirtports(res.data);
+			setAirports(res.data);
+			setOgAirports(res.data);
 			setEdited(false);
 		});
 	}, [edited]);
 
-	const handleDelete = event => {
-		const response = deleteItem(`api/airports/${event.currentTarget.name}/`);
-		response.then(() => setEdited(true));
-	};
+	useEffect(() => {
+		if (ogAirports.length > 0) {
+			let ports = ogAirports;
+			ports = ports
+				.filter(row => {
+					return row.name.toLowerCase().startsWith(nameFilter);
+				})
+				.filter(row => {
+					return row.IATA_code.toLowerCase().startsWith(iataCodeFilter);
+				})
+				.filter(row => {
+					return row.ICAO_code.toLowerCase().startsWith(icaoCodeFilter);
+				});
+			setAirports(ports);
+		}
+	}, [ogAirports, nameFilter, icaoCodeFilter, iataCodeFilter, countryFilter]);
 
-	const airportsListElements = airports.map(airport => (
-		<ListItem button key={airport.id}>
-			<ListItemText
-				primary={airport.name}
-				secondary={`IATA code: ${airport.IATA_code}, ICAO code: ${airport.ICAO_code}, countryID: ${airport.country}`}
+	useEffect(() => {
+		if (airports.length > 0) {
+			setAirports(listSort(airports, orderBy, order));
+		}
+	}, [airports, orderBy, order]);
+
+	const airportsTableRows = airports.map(airport => (
+		<TableRow key={airport.id}>
+			<TableCell>{airport.name}</TableCell>
+			<TableCell align="right">{airport.IATA_code}</TableCell>
+			<TableCell align="right">{airport.ICAO_code}</TableCell>
+			<TableCell align="right">{airport.country}</TableCell>
+			<TableActions
+				editLink={`/airlines/${airport.id}/edit`}
+				setEdited={setEdited}
+				url="api/airports"
+				itemID={airport.id}
 			/>
-			<ListItemSecondaryAction>
-				<Link to={`/airports/${airport.id}/edit`}>
-					<IconButton edge="end">
-						<Edit />
-					</IconButton>
-				</Link>
-				<IconButton edge="end" onClick={handleDelete} name={airport.id}>
-					<Delete name={airport.id} />
-				</IconButton>
-			</ListItemSecondaryAction>
-		</ListItem>
+		</TableRow>
 	));
 
+	const filters = [
+		{
+			label: "Name",
+			name: "name",
+			value: nameFilter,
+			onChange: setNameFilter
+		},
+		{
+			label: "IATA code",
+			name: "iata-code",
+			value: iataCodeFilter,
+			onChange: setIataCodeFilter
+		},
+		{
+			label: "ICAO code",
+			name: "icao-code",
+			value: icaoCodeFilter,
+			onChange: setIcaoCodeFilter
+		}
+	];
+
+	const headers = [
+		{ align: "inherit", name: "Name" },
+		{ align: "right", name: "IATA Code" },
+		{ align: "right", name: "ICAO Code" },
+		{ align: "right", name: "Country" }
+	];
+
+	const handleOrder = name => {
+		if (orderBy === name) {
+			setOrder(order === "desc" ? "asc" : "desc");
+		} else {
+			setOrder("desc");
+			setOrderBy(name);
+		}
+	};
+
 	return (
-		<div>
-			<div id="airport-utils">
-				<Link to="/airports/add-airport" className="link">
-					<Button id="add-airport-btn" variant="contained">
-						Add Airport
-					</Button>
-				</Link>
-			</div>
-			<div id="airports-list-container">
-				<List id="airports-list">{airportsListElements}</List>
-			</div>
+		<div id="airports-list-container">
+			<TableToolbar
+				tableTitle="Airports"
+				addLink="/airports/add-airport"
+				filters={filters}
+			/>
+
+			<Table>
+				<TableHeader
+					headers={headers}
+					orderBy={orderBy}
+					handleOrder={handleOrder}
+					order={order}
+				/>
+
+				<TableBody>{airportsTableRows}</TableBody>
+			</Table>
 		</div>
 	);
 };
