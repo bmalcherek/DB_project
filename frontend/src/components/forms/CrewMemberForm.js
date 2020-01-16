8615aa80080349cd7d06fd27432551e5598784c9
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2020-01-16 13:37:33 +0100

    ADD input checks to crew member form

--- frontend/src/components/forms/CrewMemberForm.js
+++ frontend/src/components/forms/CrewMemberForm.js
@@ -32,10 +32,24 @@ const CrewMemberForm = props => {
 	const [title, setTitle] = useState("");
 	const [airlines, setAirlines] = useState([]);
 	const [crews, setCrews] = useState([]);
+	const [errors, setErrors] = useState({
+		name: "",
+		airline: "",
+		crew: "",
+		job_title: ""
+	});
 
 	const classes = useStyles();
 	const { memberID } = useParams();
 	const history = useHistory();
+
+	useEffect(() => {
+		if (props.edit) {
+			document.title = "Edit Crew Member";
+		} else {
+			document.title = "Add Crew Member";
+		}
+	}, [props.edit]);
 
 	useEffect(() => {
 		if (props.edit) {
@@ -78,6 +92,17 @@ const CrewMemberForm = props => {
 		}
 
 		response.then(() => history.push("/crew-members"));
+		response.catch(err => {
+			let newErrors = {};
+			for (var key in errors) {
+				if (key in err.response.data) {
+					newErrors[key] = err.response.data[key][0];
+				} else {
+					newErrors[key] = "";
+				}
+			}
+			setErrors(newErrors);
+		});
 	};
 
 	const airlinesOptions = airlines.map(airline => (
@@ -99,7 +124,7 @@ const CrewMemberForm = props => {
 	return (
 		<div id="form-container">
 			<Typography>
-				<b>Add New Crew Member</b>
+				{props.edit ? "Edit Crew Member" : "Add Crew Member"}
 			</Typography>
 
 			<FormControl className={classes.formControl}>
@@ -110,10 +135,12 @@ const CrewMemberForm = props => {
 					id="name-field"
 					label="Name"
 					variant="outlined"
+					error={errors.name.length > 0}
+					helperText={errors.name}
 				/>
 			</FormControl>
 
-			<FormControl className={classes.formControl} syle={{ width: 200 }}>
+			<FormControl className={classes.formControl} style={{ width: 235 }}>
 				<InputLabel id="airline-select-label">Airline</InputLabel>
 				<Select
 					labelId="airline-select-label"
@@ -121,17 +148,23 @@ const CrewMemberForm = props => {
 					name="airline"
 					value={airline}
 					onChange={event => setAirline(event.target.value)}
+					error={errors.airline.length > 0}
 				>
 					{airlinesOptions}
 				</Select>
 				<FormHelperText>
+					{errors.airline.length.length > 0 ? (
+						<Typography style={{ color: "#f44336", fontSize: 12 }}>
+							{errors.airline}
+						</Typography>
+					) : null}
 					<Link to="/airline/add-airline" className="link">
 						If airline not visible click here
 					</Link>
 				</FormHelperText>
 			</FormControl>
 
-			<FormControl className={classes.formControl} style={{ width: 200 }}>
+			<FormControl className={classes.formControl} style={{ width: 235 }}>
 				<InputLabel id="crew-select-label">Crew</InputLabel>
 				<Select
 					labelId="crew-select-label"
@@ -139,10 +172,16 @@ const CrewMemberForm = props => {
 					name="crew"
 					value={crew}
 					onChange={event => setCrew(event.target.value)}
+					error={errors.crew.length > 0}
 				>
 					{crewsOptions}
 				</Select>
 				<FormHelperText>
+					{errors.crew.length > 0 ? (
+						<Typography style={{ color: "#f44336", fontSize: 12 }}>
+							{errors.crew}
+						</Typography>
+					) : null}
 					<Link to="/crews/add-crew" className="link">
 						If no crew visible, add crew to selected airline here
 					</Link>
@@ -157,6 +196,8 @@ const CrewMemberForm = props => {
 					id="title-field"
 					label="Job Title"
 					variant="outlined"
+					error={errors.job_title.length > 0}
+					helperText={errors.job_title}
 				/>
 			</FormControl>
 
